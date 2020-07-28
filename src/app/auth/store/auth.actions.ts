d49691f10c926998cd6e6a27be59fc9b7effc726
Storm3r <iamstorm3r@gmail.com>
2020-07-28 17:30:45 +0300

    [Created] Auth store structure

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,61 @@
+import { Action } from '@ngrx/store';
+
+export const LOGIN_START = '[Auth] Login Start';
+export const AUTHENTICATION_SUCCESS = '[Auth] Authentication Success';
+export const AUTHENTICATION_FAIL = '[Auth] Authentication Fail';
+export const SIGNUP_START = '[Auth] Signup Start';
+export const LOGOUT = '[Auth] Logout';
+export const AUTO_LOGIN = '[Auth] Auto Login';
+
+
+export class AuthenticationSuccess implements Action {
+    readonly type = AUTHENTICATION_SUCCESS;
+
+    constructor(public payload: {
+        email: string,
+        idToken: string,
+        refreshToken: string,
+        expiresIn: string,
+        localId: string;
+    }) {}
+}
+
+export class AuthenticationFail implements Action {
+    readonly type = AUTHENTICATION_FAIL;
+}
+
+export class SignupStart implements Action {
+    readonly type = SIGNUP_START;
+
+    constructor(public payload: {
+        email: string,
+        password: string
+    }){}
+}
+
+export class LoginStart implements Action {
+    readonly type = LOGIN_START;
+
+    constructor(public payload: {
+        email: string,
+        password: string
+    }) {}
+}
+
+export class AutoLogin implements Action {
+    readonly type = AUTO_LOGIN;
+}
+
+export class Logout implements Action {
+    readonly type = LOGOUT;
+}
+
+
+export type AuthActions =
+  LoginStart
+| AuthenticationSuccess
+| AuthenticationFail
+| SignupStart
+| AutoLogin
+| Logout;
+
